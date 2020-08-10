b10917be836c073f9c933f82effa39d45d30cc1b
Fredbaltimore <fredinmd410@gmail.com>
2020-08-09 21:53:44 -0400

    Update script.js
    just cleaning my code up

--- script.js
+++ script.js
@@ -1,54 +1,50 @@
-let button = document.getElementById("enter");
-let input = document.getElementById("userinput");
 let ul = document.querySelector("ul");
+let button = document.getElementById("enter");
+let userInput = document.getElementById("userinput");
 
-
-function removeParent(evt){
-	evt.target.removeEventListener("click", removeParent, false);
-	evt.target.parentNode.remove();
+function deleteTask(evt){
+    evt.target.removeEventListener("click", deleteTask, false);
+    evt.target.parentNode.remove();
+}
+function userInputLength(){
+   return userInput.value.length;
 }
 
-function getEventTarget(e){
-	e = e || window.event;
-	return e.target || e.srcElement;
+function getEventTarget (evt){
+    this.event;
+    return evt.target;
 }
-
 ul.onclick = function(event){
-	let target = getEventTarget(event);
-	target.classList.toggle("done");
+    let target = getEventTarget(event);
+    target.classList.toggle("done");
 }
 
+function createTask(){
+    let deleteButton = document.createElement("button");
+    deleteButton.innerHTML = "Delete";
+    let li = document.createElement("li");
 
-function inputLength() {
-	return input.value.length;
+    li.appendChild(document.createTextNode(userInput.value));
+    li.innerHTML = li.innerHTML + " ";
+    li.appendChild(deleteButton);
+    deleteButton.onclick = deleteTask;
+    ul.appendChild(li);
+    userInput.value = "";
+    
 }
 
-function createListElement() {
-	let deleteButton = document.createElement("button");
-	deleteButton.innerHTML = "Delete";
-	
-
-	let li = document.createElement("li");
-	li.appendChild(document.createTextNode(input.value));
-	li.innerHTML = li.innerHTML + " ";
-	li.appendChild(deleteButton);
-	deleteButton.onclick = removeParent;
-	ul.appendChild(li);
-	input.value = "";
+function addTaskAfterKeypress(evt){
+    if (userInputLength() > 0 && evt.which === 13){
+        createTask();
+    }
 }
 
-function addListAfterClick() {
-	if (inputLength() > 0) {
-		createListElement();
-	}
+function addTaskAfterClick(){
+    if (userInputLength() > 0){
+        createTask();
+
+    }
 }
 
-function addListAfterKeypress(event) {
-	if (inputLength() > 0 && event.keyCode === 13) {
-		createListElement();
-	}
-}
-
-button.addEventListener("click", addListAfterClick);
-
-input.addEventListener("keypress", addListAfterKeypress);
+button.addEventListener("click", addTaskAfterClick);
+userInput.addEventListener("keypress", addTaskAfterKeypress);
