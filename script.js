3ef2b44dda3f7be66a0da692b371768379788e79
Fredbaltimore <fredinmd410@gmail.com>
2020-08-26 23:42:52 -0400

    cleaning code up
    just making some small changes to codecase

--- script.js
+++ script.js
@@ -1,49 +1,47 @@
-let ul = document.getElementById("todos");
-let button = document.getElementById("enter");
-let userInput = document.getElementById("userinput");
+const ul = document.getElementById("todos");
+const button = document.getElementById("enter");
+const userInput = document.getElementById("userinput");
 
-function deleteTask(evt){
-    evt.target.removeEventListener("click", deleteTask, false);
-    evt.target.parentNode.remove();
+function deleteTask(evt) {
+  evt.target.removeEventListener("click", deleteTask, false);
+  evt.target.parentNode.remove();
 }
-function userInputLength(){
-   return userInput.value.length;
-}
-
-function getEventTarget (evt){
-    this.event;
-    return evt.target;
-}
-ul.onclick = function(event){
-    let target = getEventTarget(event);
-    target.classList.toggle("done");
+function userInputLength() {
+  return userInput.value.length;
 }
 
-function createTask(){
-    let deleteButton = document.createElement("button");
-    deleteButton.innerHTML = "Delete";
-    let li = document.createElement("li");
+function getEventTarget(evt) {
+  this.event;
+  return evt.target;
+}
+ul.onclick = function (event) {
+  let target = getEventTarget(event);
+  target.classList.toggle("done");
+};
 
-    li.appendChild(document.createTextNode(userInput.value));
-    li.innerHTML = li.innerHTML + " ";
-    li.appendChild(deleteButton);
-    deleteButton.onclick = deleteTask;
-    ul.appendChild(li);
-    userInput.value = "";
-    
+function createTask() {
+  let deleteButton = document.createElement("button");
+  deleteButton.innerHTML = "Delete";
+  let li = document.createElement("li");
+
+  li.appendChild(document.createTextNode(userInput.value));
+  li.innerHTML = li.innerHTML + " ";
+  li.appendChild(deleteButton);
+  deleteButton.onclick = deleteTask;
+  ul.appendChild(li);
+  userInput.value = "";
 }
 
-function addTaskAfterKeypress(evt){
-    if (userInputLength() > 0 && evt.which === 13){
-        createTask();
-    }
+function addTaskAfterKeypress(evt) {
+  if (userInputLength() > 0 && evt.which === 13) {
+    createTask();
+  }
 }
 
-function addTaskAfterClick(){
-    if (userInputLength() > 0){
-        createTask();
-
-    }
+function addTaskAfterClick() {
+  if (userInputLength() > 0) {
+    createTask();
+  }
 }
 
 button.addEventListener("click", addTaskAfterClick);
